b93cb91d052d28b51e9049d043615aea0ec65069
Youssef Moawad <youssef_programming@icloud.com>
2023-07-18 14:18:14 +0100

    Initial solution to algs prob 3 in python

---
+++ algorithms/3.LongestSubstringNoRepeats/python/main.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = len(s)
+        substring = ""
+        longest_substring = ""
+        charIndexMap = {}
+
+        for i in range(count):
+            print(charIndexMap)
+            print(substring)
+            if s[i] in substring:
+                if len(substring) > len(longest_substring):
+                   longest_substring = substring 
+                substring = s[charIndexMap[s[i]]:i]
+                charIndexMap[s[i]] = i
+            else:
+                charIndexMap[s[i]] = i
+                substring += s[i]
+        if len(substring) > len(longest_substring):
+            longest_substring = substring
+        return len(longest_substring)
+    
+sol = Solution()
+print(sol.lengthOfLongestSubstring("aab"))
